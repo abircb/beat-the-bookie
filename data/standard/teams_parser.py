9c6a0f03c239c940c62afe439ec6f0d9ce9f0f7d
abir <abirchandrabhushan@gmail.com>
2021-01-19 00:08:54 +0000

    add non-shot xG model with standardised mappings for str parameters

--- data/standard/teams_parser.py
+++ data/standard/teams_parser.py
@@ -4,12 +4,15 @@ import pandas as pd
 
 
 def extract_team_names():
-    FILENAME = "../football-data.co.uk/updated-epl-training.csv"
+    FILENAME = os.path.join(
+        os.getcwd(), "../football-data.co.uk/updated-epl-training.csv"
+    )
 
     def parse_csv():
         teams = set()
         for line in open(FILENAME):
-            teams.add(line.split(",")[2])
+            teams.add(line.split(",")[2])  # home teams
+            teams.add(line.split(",")[3])  # away teams
         return sorted(list(teams))
 
     return parse_csv()
@@ -18,6 +21,10 @@ def extract_team_names():
 if __name__ == "__main__":
     team_names = {"Standard teamname": extract_team_names()}
     data = pd.DataFrame(data=team_names)
+
+    # give each team a unique ID
+    unique_ID = [i for i in range(1, 1 + data["Standard teamname"].count())]
+    data.insert(0, "TeamID", unique_ID)
 
     path = os.path.join(os.getcwd(), "standard.teamnames.csv")
     data.to_csv(path, index=False)
